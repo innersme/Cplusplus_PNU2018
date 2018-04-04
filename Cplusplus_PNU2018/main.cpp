3728221c77ef1488b7176a83ec8a64fca6b39941
KIMMINJAE <innersme@gmail.com>
2018-04-04 15:30:38 +0900

    GitHub for git merge

--- Cplusplus_PNU2018/main.cpp
+++ Cplusplus_PNU2018/main.cpp
@@ -12,5 +12,6 @@ int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
     std::cout << "Hello, Local repository";
+    std::cout << "Hello, GitHub!\n";
     return 0;
 }
