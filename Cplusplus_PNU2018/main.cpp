b7061c1afc4a8a62298bf5ba6e37f3b94056249c
KIMMINJAE <innersme@gmail.com>
2018-04-04 15:06:04 +0900

    Initial Commit

---
+++ Cplusplus_PNU2018/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Cplusplus_PNU2018
+//
+//  Created by MJMacBook on 04/04/2018.
+//  Copyright © 2018 MJMacBook. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
